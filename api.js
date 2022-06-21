aa7574d27a0f4446e3e7332090ac4f36b32b95c4
AMIROUCHEN Massyl <hm_amirouchen@esi.dz>
2022-06-21 22:32:02 +0100

    Fixing get closest parkings.

--- api.js
+++ api.js
@@ -108,16 +108,15 @@ app.get('/parkings', async function (req, res){
 })
 
 app.get('/parkings/closest', async function (req, res ){
-  const query = "SELECT * FROM (SELECT *, \n" +
-      "SQRT( POW( ( (69.1/1.61) * ('$1' - latitude)), 2) \n" +
-      "+ POW(( (53/1.61) * ('$2' - longitude)), 2)) AS distance \n" +
-      "FROM public.parkings) as p, \n" +
-      "TO_JSON(ARRAY (SELECT \n" +
-      "\t   (day,to_char(opening_hour, 'HH24:MI:SS'),to_char(closing_hour,'HH24:MI:SS'))\n" +
-      "\t   FROM public.schedules\n" +
-      "\t  WHERE parking_schedule = parking_id)) as schedule\n" +
-      "FROM public.parkings) \n" +
-      "WHERE distance < 3";
+  const query =
+      "SELECT * FROM (SELECT *, \n" +
+      "\t\t\t   SQRT( POW( ( (69.1/1.61) * ('36.709713' - latitude)), 2)\n" +
+      "               + POW(( (53/1.61) * ('3.051708' - longitude)), 2)) AS distance,\n" +
+      "\t\t\t   TO_JSON(ARRAY (SELECT (day,to_char(opening_hour, 'HH24:MI:SS'),to_char(closing_hour,'HH24:MI:SS')) \n" +
+      "\t\t\t\t\t\t\t  FROM public.schedules\n" +
+      "\t\t\t\t\t\t\t  WHERE parking_schedule = parking_id)) as schedule\n" +
+      "\t\t\t   FROM public.parkings) as p\n" +
+      "\t\t\t   WHERE p.distance < 10"
   try {
     let result = await client.query(query, [req.query.latitude, req.query.longitude])
     res.status(200).json({message:"Success", result: result})
